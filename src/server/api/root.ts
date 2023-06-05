8cbc7c2c612144a1e795cecf1243cfaf24da2572
Yuri Ramos <yuri.pramos@gmail.com>
2023-06-05 17:08:23 -0300

    Update root.ts

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { postRouter } from "~/server/api/routers/post";
+import { postRouter } from "~/server/api/routers/posts";
 
 /**
  * This is the primary router for your server.
