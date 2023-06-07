611c8b9444cdfcf6fec1eb308bd8b9789d2f4d4a
Yuri Ramos <yuri.pramos@gmail.com>
2023-06-07 12:12:57 -0300

    Add profile API router

---
+++ src/server/api/routers/profile.ts
@@ -0,0 +1,48 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+
+import { clerkClient, type User } from "@clerk/nextjs/server";
+import { TRPCError } from "@trpc/server";
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    profileImageUrl: user.profileImageUrl,
+  };
+};
+
+export const profileRouter = createTRPCRouter({
+  getUserByUsername: publicProcedure
+    .input(z.object({ username: z.string() }))
+    .query(async ({ input }) => {
+      const [user] = await clerkClient.users.getUserList({
+        username: [input.username],
+      });
+
+      if (!user) {
+        // if we hit here we need a unsantized username so hit api once more and find the user.
+        const users = await clerkClient.users.getUserList({
+          limit: 200,
+        });
+        const user = users.find((user) =>
+          user.externalAccounts.find(
+            (account) => account.username === input.username
+          )
+        );
+        if (!user) {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "User not found",
+          });
+        }
+        return filterUserForClient(user);
+      }
+
+      return filterUserForClient(user);
+    }),
+});
